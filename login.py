98accfa06f1af4358b4ee68a4deba05446426fd4
Anika Lakhani <anikalakhani@college.harvard.edu>
2023-02-17 02:31:40 -0500

    all cleaned up

--- login.py
+++ login.py
@@ -1,9 +1,18 @@
 from stdiomask import getpass
 import hashlib
 import os
+import time
 clear = lambda: os.system('cls')
 
 
+
+# MAIN: This is kind of like the CLI landing page for the application.
+    ## From here, the user has two main functionalities: Register and Log In.
+    ## If the user types '1', they will be prompted to register for a new account.
+    ## If the user types '2', they will be prompted to log into an existing account.
+    ## If the user types '3', they will actually also go to the login page.
+    ## But, they will first see a message that shows how to delete an account.
+    ## For security purposes, one can only delete an account once they are logged in.
 def main():
 
     # Rendering initial visuals; presenting users with login options
@@ -31,7 +40,9 @@ def main():
         Login()
 
     elif userChoice == '3': # existing user wants to delete their account
-        Delete()
+        print("First log in, then type '/delete account' to delete your account.")
+        time.sleep(2000) # delays the redirection for 2 seconds so that users have time to read the message
+        Login()
 
 
 
@@ -56,7 +67,7 @@ def Register():
     userName = sanitizeName(userName) # saves username as a string
 
     if userAlreadyExist(userName): # checks if existing user
-        displayUserAlreadyExistMessage()
+        displayExistenceMessage()
 
     else: # this is a new user with a valid username
 
@@ -81,12 +92,16 @@ def Register():
 
         print()
         print("Registered! (:)>")
+        time.sleep(2000) # delays the redirection for 2 seconds so that users have time to read the message
+
+        print("If you would like to delete your account, please type '/delete account' at any time.")
+        time.sleep(2000) # delays the redirection for 2 seconds so that users have time to read the message
 
 
 
 # LOGIN: Allows an existing user to log into an account that has already been made and entered into our database.
     ## User must enter valid username and password
-    ## After 10 failed login attempts, account will be automatically deleted for security purposes
+    ## After 10 failed login attempts, user will be encouraged to register for a new account
 def Login():
 
     # Render initial graphics
@@ -140,82 +155,100 @@ def Login():
     # User has successfully logged in; print success message    
     print()
     print("Logged In! (:)>")
-
-def Delete():
-    clear()
-    print("DELETE ACCOUNT")
-    print("--------")
-    print()
-    while True:
-        userName = input("Enter Username: ").title()
-        if userName != '':
-            break
-    userName = sanitizeName(userName)
-    if userAlreadyExist(userName):
-        confirm = input("Are you sure you want to delete? Type (Y) for yes and (N) for no: ").lower()
-        if confirm == 'y':
-            rmUserInfo()
-        elif confirm != 'n':
-            print("Please enter a valid input.")
+    time.sleep(2000) # delays the redirection for 2 seconds so that users have time to read the message
 
 
+
+# ADDUSERINFO: Appends the user's information to the txt file with all logins and passwords
 def addUserInfo(userInfo: list):
     with open('userInfo.txt', 'a') as file:
         for info in userInfo:
             file.write(info)
-            file.write(' ')
+            file.write(' ') ## QUESTION: WHY DO WE NEED THE SPACE
         file.write('\n')
 
-def rmUserInfo(username, userInfo: list):
-    with open('userInfo.txt', 'r') as input:
-        with open('temp.txt', 'w') as output:
-            for user in input:
-                if username not in user.strip(""):
-                    output.write(user)
-    
-    os.replace('temp.txt', 'userInfo.txt')
 
+
+# USERALREADYEXIST: Given a username, checks if an account exists under a specific username and password
+    ## If a username has not been used yet, the function will return false
+
+## QUESTION: I DON'T UNDERSTAND WHY WE NEED THE 'NONE' AT THE BEGINNING
 def userAlreadyExist(userName, userPassword=None):
+    
     if userPassword == None:
+
+        ## QUESTION: explain what this does
         with open('userInfo.txt', 'r') as file:
             for line in file:
                 line = line.split()
+
                 if line[0] == userName:
                     return True
+
         return False
+
+    # QUESTION: How do we even get to the else statement if we're initializing userPassword as none?
     else:
-        userPassword = hash_password(userPassword)
-        usersInfo = {}
+        userPassword = hash_password(userPassword) # hashes the password for enhanced security
+        usersInfo = {} # initializes an empty dictionary for which to store the user info
+
+        # Fills in our empty dictionary with the user's info
         with open('userInfo.txt', 'r') as file:
             for line in file:
                 line = line.split()
                 if line[0] == userName and line[1] == userPassword:
                     usersInfo.update({line[0]: line[1]})
+        
+        # usersInfo will remain blank if the user's information was not found in the .txt document
         if usersInfo == {}:
-            return False
+            return False # therefore, we know that the user does not yet exist
+
+        # QUESTION: WHY IS THIS THE RETURN VALUE    
         return usersInfo[userName] == userPassword
 
-def displayUserAlreadyExistMessage():
+
+
+# DISPLAYEXISTENCEMESSAGE: For use if the user is trying to register but already has an account.
+    ## Presents the user with more helpful options since they were falsely trying to register.
+    ## If the user presses T, they will be able to register as a new user.
+    ## If the user presses L, they will be able to log into their existing account.
+def displayExistenceMessage():
     while True:
+        # Setting up the graphics
         print()
-        error = input("You Are Already Registered.\n\nPress (T) To Try Again:\nPress (L) To Login:\nPress (D) To Delete: ").lower()
+        error = input("You Are Already Registered.\n\nPress (T) To Try Again:\nPress (L) To Login: ").lower()
+
         if error == 't':
             Register()
             break
+
         elif error == 'l':
             Login()
             break
-        elif error == 'n':
-            Delete()
+
+        else: # the user has provided undesired input (they are probably confused and should be redirected)
+            print("Redirecting you to the main menu.")
+            main()
             break
 
+
+
+# SANITIZENAME: Reformats the username to our standard for more standardized recording in the .txt document.
+    ## Example: The string 'mallick prat' would become 'mallick-prat'
 def sanitizeName(userName):
-    userName = userName.split()
-    userName = '-'.join(userName)
+    userName = userName.split() # 'mallick prat' --> ['mallick', 'prat']
+    userName = '-'.join(userName) # ['mallick', 'prat'] --> 'mallick-prat'
     return userName
 
+
+
+# SPAMLOGINS: Used when the user has tried logging in, either by entering an incorrect username or an incorrect password 10+ times.
+    ## Since the user is repeatedly trying a path that is not helpful to them, they will be redirected.
+    ## The user is prompted to create a new account since they likely don't have one.
+    ## If the user selects 'N,' they will be redirected to the main menu.
 def spamLogins():
 
+    clear()
     print("You have attempted too many times. Would you like to create an account?")
     
     while True: # makes sure user has given correct input
@@ -230,10 +263,21 @@ def spamLogins():
         print("Redirecting back to main options.")
         main()
 
+
+
+# HASH_PASSWORD: Uses SHA256 encryption to hash the password for enhanced security
 def hash_password(password):
     return hashlib.sha256(str.encode(password)).hexdigest()
 
+
+
+# CHECK_PASSWORD_HASH: Given a password and its hash, return a boolean for if the password is correct.
+    ## With hash encryption, only the correct password will produce the unique hash.
+    ## So, a false password will make the return statement false and vice versa.
 def check_password_hash(password, hash):
     return hash_password(password) == hash
-    
+
+
+
+# To start, display the 'main menu' in the CLI for the user.
 main()
