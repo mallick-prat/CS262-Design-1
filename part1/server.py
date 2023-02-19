b813a5278c1c39316df9e5ed7582aeec87c53bc5
PRATYUSH MALLICK <pratmallick@college.harvard.edu>
2023-02-19 15:02:26 -0500

    removing login file -> consolidating all to client. Added global user functionality.

--- part1/server.py
+++ part1/server.py
@@ -1,31 +1,60 @@
 import socket
+import threading
+#localhost / add functionality to define host/port connection --> ergo web server between different machines. 
+host = '127.0.0.1'
+port = 55556
 
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
 
-def server_program():
-    # get the hostname
-    host = socket.gethostname()
-    port = 8000  # initiate port no above 1024
+# Lists For Clients and Their Nicknames
+clients = []
+usernames = []
 
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
-
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
+# receive message from client, then broadcast to other clients; run this function in each thread per client. 
+def handle(client):
     while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            username = usernames[index]
+            broadcast('{} left!'.format(username).encode('ascii'))
+            usernames.remove(username)
             break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
 
-    conn.close()  # close the connection
+def broadcast(message):
+    for client in clients:
+        client.send(message)
 
 
-if __name__ == '__main__':
-    server_program()
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('ascii'))
+        username = client.recv(1024).decode('ascii')
+        usernames.append(username)
+        clients.append(client)
+
+        # Print And Broadcast Nickname
+        print("Username is {}".format(username))
+        broadcast("{} joined!".format(username).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server is listening for new connections...")
+receive()
