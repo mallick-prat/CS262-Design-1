9b389d3823ab8836eeabe277377299a714f67da6
Anika Lakhani <anikalakhani@college.harvard.edu>
2023-04-07 03:04:52 -0400

    go to 'backup' directory, half-working #2, look in server.py

--- backup/part1/server.py
+++ backup/part1/server.py
@@ -17,6 +17,7 @@ server.listen()
 # Lists For Clients and Their Usernames
 clients = []
 usernames = []
+current_msg = []
 
 # Read the current session ID from the file
 with open('session_id.txt', 'r') as f:
@@ -27,7 +28,21 @@ def handle(client):
     while True:
         try:
             # Broadcasting Messages
+            new_message = False
             msg = message = client.recv(1024)
+            
+            full_msg_text = msg.decode('ascii')
+            msg_text = full_msg_text.split(": ")[1]
+            print(msg_text)
+            current_msg.append(msg_text)
+            print("The length of current_msg is " + str(len(current_msg)))
+
+            new_message = True
+
+            if new_message:
+                sql_message(client, current_msg[-1])
+                new_message = False
+            
             if msg.decode('ascii').startswith('DELETE'):
                 accountDeleted = msg.decode('ascii')[5:]
                 print(accountDeleted)
@@ -101,14 +116,20 @@ def receive():
         # Print And Broadcast Username
         print("Username is {}".format(username))
         broadcast("{} joined!".format(username).encode('ascii'))
+        #broadcast()
         client.send('Connected to server!'.encode('ascii'))
+
+        #if len(current_msg) > 0:
+            #sql_message(client, current_msg[-1])
 
         # Start Handling Thread For Client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
-        msg = client.recv(1024)
-        sql_message(client, msg)
+        #msg = client.recv(1024).decode('ascii')
+        print("Bottom: the length of current_msg is " + str(len(current_msg)))
+        # if len(current_msg) > 0:
+            # sql_message(client, current_msg[-1])
 
 # Increment the session ID and write it back to the file
 session_id += 1
